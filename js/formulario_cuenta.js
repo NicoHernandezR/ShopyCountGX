776cf0247da17664e24643ad1a1372cdc8dbf0d2
NicoHernandezR <nic.hernandezr@duocuc.cl>
2023-04-23 22:49:24 -0400

    Version_2.3.1

--- js/formulario_cuenta.js
+++ js/formulario_cuenta.js
@@ -5,13 +5,12 @@ let selectedImages = [];
 const tiposCuenta = ["Seleccionar","Steam", "CSGO", "Epic", "PlayStation","Xbox Live"];
 const opcionesCuenta = {
   "Seleccionar": [],
-  "Steam": ["Juegos", "Nivel Cuenta"],
-  "CSGO": ["MM", "GC", "Faceit"],
-  "Epic": ["Juegos"],
-  "PlayStation": ["Juegos", "Suscripciones", "Tarjetas de regalo"],
-  "Xbox Live": ["Juegos", "Suscripciones"]
+  "Steam": [["Juegos",0,3000], ["Nivel Cuenta",0,1000]],
+  "CSGO": [["MM", 0, 18], ["GC",0,21], ["Faceit",0,10]],
+  "Epic": [["Juegos",0,3000]],
+  "PlayStation": [["Juegos",0,3000], ["Suscripciones",0,12]],
+  "Xbox Live": [["Juegos",0,3000], ["Suscripciones",0,12]]
 };
-
 
 
 
@@ -41,8 +40,7 @@ window.onload = function() {
     let btn_img = document.getElementById("btn_img");
     let precio = document.getElementById("precio")
     let precio_dv = document.getElementById("precio_dv")
-    console.log(txtExtra)
-    
+
     tiposCuenta.forEach(function(tipo, index) {
       let option = document.createElement("option");
       option.value = index + 1;
@@ -56,7 +54,7 @@ window.onload = function() {
       opcionesCuentas.innerHTML = ""
       // Obtener las opciones correspondientes del objeto opcionesCuenta
       let opcionesCuentaSeleccionada = opcionesCuenta[tiposCuenta[tipoCuentaSeleccionada - 1]];
-      console.log(opcionesCuentaSeleccionada)
+
     
       // Crear el segundo select
     
@@ -66,9 +64,14 @@ window.onload = function() {
         let inp = document.createElement("input");
         let div = document.createElement("div");
         
-        lbl.textContent = option;
+        lbl.textContent = option[0];
         inp.type = "number"
-        inp.name = option;
+        inp.id = option[0];
+        inp.name = option[0];
+        inp.min = option[1];
+        inp.max = option[2];
+        inp.value = inp.min;
+    
     
         lbl.classList.add("col-12")
         inp.classList.add("col-12")
@@ -95,8 +98,6 @@ window.onload = function() {
         txtExtra.classList.remove("hidden")
         btn_img.classList.remove("hidden")
         precio_dv.classList.remove("hidden")
-        contador = contador + 1;
-        console.log(contador)
       } else {
         txtExtra.value = ""
         precio.value = ""
@@ -137,7 +138,7 @@ window.onload = function() {
          // Agregar el archivo a la matriz de imágenes seleccionadas
 
       }
-      console.log(selectedImages); // Mostrar las imágenes seleccionadas en la consola
+       // Mostrar las imágenes seleccionadas en la consola
       // Aquí puedes enviar las imágenes al servidor o hacer lo que necesites con ellas.
     });
 
@@ -145,3 +146,94 @@ window.onload = function() {
   };
 
 
+
+function validarFormulario() {
+
+  let tCuenta = document.getElementById('tipoCuenta');
+  let tcuenta2 = tiposCuenta[tCuenta.value - 1]
+  let precio = document.getElementById('precio');
+  let infoExtra = document.getElementById('info_extra');
+  let arrayDatos = opcionesCuenta[tcuenta2]
+  let arrayErrores = []
+  
+  console.log(tCuenta.value)
+  console.log(tiposCuenta[tCuenta.value - 1])
+  console.log(arrayDatos)
+
+
+  if (arrayDatos.length < 1){
+    alert("Seleccione algun tipo de cuenta a vender.");
+    return false;
+  }
+
+  arrayDatos.forEach(function(e, ix) {
+
+    let hname = e[0]
+    let html_comp = document.getElementById(hname)
+    let hvalue = parseInt(html_comp.value); 
+    let hmin = parseInt(html_comp.min) 
+    let hmax = parseInt(html_comp.max) 
+
+    console.log(hvalue)
+    if (hvalue < hmin){
+      arrayErrores.push('El valor de ' + hname + ' no puede ser menor a ' + hmin);
+    }else if(hvalue > hmax){
+      arrayErrores.push('El valor de ' + hname + ' no puede ser mayor a ' + hmax);
+    }
+  });
+
+  if (parseInt(precio.value) <= parseInt(precio.min)){
+    arrayErrores.push('El valor del precio no puede ser menor a ' + precio.min);
+  }else if(parseInt(precio.value)  > parseInt(precio.max)){
+    arrayErrores.push('El valor del precio no puede ser mayor a ' + precio.max);
+  }
+
+  if (infoExtra.value === ''){
+    arrayErrores.push('Tiene que proporcionar informacion extra acerca de la cuenta');
+  }
+
+  if (selectedImages.length < 1){
+    arrayErrores.push('Suba entre 1 a 5 imagenes para dar informacion sobre la cuenta')
+  }
+
+  if (arrayErrores.length > 0){
+    alert(arrayErrores.join('. \n'))
+    return false;
+  }
+
+  return true;
+
+
+  }
+
+function limpiarFormulario() {
+  let tCuenta = document.getElementById('tipoCuenta');
+  let tcuenta2 = tiposCuenta[tCuenta.value - 1]
+  let precio = document.getElementById('precio');
+  let infoExtra = document.getElementById('info_extra');
+  let arrayDatos = opcionesCuenta[tcuenta2]
+
+  let txtExtra = document.getElementById("textArea")
+  let btn_img = document.getElementById("btn_img");
+  let precio_dv = document.getElementById("precio_dv")
+  let opCuenta = document.getElementById('opcionesCuenta')
+
+  arrayDatos.forEach(function(e, ix) {
+
+    let html_comp = document.getElementById(e[0])
+    html_comp.value = html_comp.min
+
+
+
+  });
+
+  precio.value = precio.min;
+  infoExtra.value = ''
+  selectedImages = []
+  tCuenta.value = 1
+  txtExtra.classList.add("hidden")
+  btn_img.classList.add("hidden")
+  precio_dv.classList.add("hidden")
+  opCuenta.innerHTML = ''
+
+}
