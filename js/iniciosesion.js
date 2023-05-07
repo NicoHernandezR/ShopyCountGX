67501d097f4a4e8f95f318dad37b1c2775859cc5
Francisco Lazcano ruminot <franciscolazcano178@gmail.com>
2023-05-06 20:07:37 -0400

    2.2

--- js/iniciosesion.js
+++ js/iniciosesion.js
@@ -1,26 +1,12 @@
-/*const username = document.getElementById("user")
-const pass = document.getElementById("password")
-const form = document.getElementById("form")
-
-
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings = ""
-    let entrar = false
-    parrafo.innerHTML = ""
-    if(username.value.length <6){
-        warnings += `El nombre no es valido <br>`
-        entrar = true
+function validateForm() {
+    var username = document.forms["form"]["username"].value;
+    var password = document.forms["form"]["password"].value;
+    if (password == "") {
+      alert("Por favor complete todos los campos");
+      return false;
     }
-
-    if(pass.value.length !== ""){
-        warnings += `La contraseña no es valida>`
-        entrar = true
+    if (username == "") {
+      alert("Por favor complete todos los campos");
+      return false;
     }
-
-    if(entrar){
-        parrafo.innerHTML = warnings
-    }else{
-        parrafo.innerHTML = "Enviado"
-    }
-})*/
+  }
