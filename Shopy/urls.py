f907e757ff9bf37dc39465efef20e87f3c3ba4a8
Francisco Lazcano ruminot <franciscolazcano178@gmail.com>
2023-06-17 13:09:52 -0400

    et33

--- Shopy/urls.py
+++ Shopy/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('registro', views.registro, name='registro'),
-     path('iniciodesesion', views.iniciodesesion, name='iniciodesesion'),
+    path('iniciodesesion', views.iniciodesesion, name='iniciodesesion'),
     #path('alumnosList', views.alumnosList, name='alumnosList'),
     #path('alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
 ]
