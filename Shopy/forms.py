1fde178e0f65a9fb5eb6307e91e1403364743a96
Francisco Lazcano ruminot <franciscolazcano178@gmail.com>
2023-06-18 22:13:57 -0400

    version 2.2

--- Shopy/forms.py
+++ Shopy/forms.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class CrearUsuario(UserCreationForm):
     pass
+    nombre = forms.CharField(label='Nombre', max_length=100)
+    correo = forms.EmailField(label='Correo electrónico')
+    contraseña = forms.CharField(label='Contraseña', max_length=100, widget=forms.PasswordInput)
