ca707e21f81dc5683b079488919337cfc3009b01
NicoHernandezR <107136793+NicoHernandezR@users.noreply.github.com>
2023-07-05 22:41:05 -0400

    Ultimos Retoques Para el Examen V2.

--- ShopyCount/urls.py
+++ ShopyCount/urls.py
@@ -17,11 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Shopy/',include('Shopy.urls')),
-    path('Shopy/', include('django.contrib.auth.urls'))
+    path('Shopy/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login')
 
 ]
 
